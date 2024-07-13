cc861bed53d739de6da93ebbf939038957a479de
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2024-07-13 16:22:36 -0400

    fix next wallpaper script bug

--- scripts/next_wallpaper.py
+++ scripts/next_wallpaper.py
@@ -4,7 +4,7 @@ import subprocess
 
 def choose_random_png(directory):
     # Get a list of all PNG files in the specified directory
-    png_files = [f for f in os.listdir(directory) if f.endswith('.png')]
+    png_files = os.listdir(directory)
     if not png_files:
         raise ValueError(f"No PNG files found in {directory}")
 
@@ -13,12 +13,11 @@ def choose_random_png(directory):
     return os.path.join(directory, random_png)
 
 def set_wallpaper_and_terminal_bg(png_file):
-    subprocess.run(f'swww img {png_file}', shell=True)
-    subprocess.run(f'wallust run {png_file}', shell=True)
+    subprocess.run(f'wallust run "{png_file}"&swww img "{png_file}" -t grow --transition-pos 0.75,0.6 --transition-bezier 0.0,0.5,0.5,1.0,5.0&', shell=True)
 
 if __name__ == '__main__':
     # Directory containing PNG wallpapers
-    wallpaper_directory = os.path.expanduser('~/Pictures/Wallpaper-Bank/wallpapers/')
+    wallpaper_directory = os.path.expanduser('~/Pictures/wallpapers/')
 
     try:
         # Choose a random PNG file
