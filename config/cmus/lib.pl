fee6530a4cdc4101d52a1946cc728a3aff3bb77a
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-01-01 10:18:34 -0500

    songs added

--- config/cmus/lib.pl
+++ config/cmus/lib.pl
@@ -1,40 +1,37 @@
-/home/heinzketchup/Music/850907_Hinkik---Explorers-Chiptun.mp3
-/home/heinzketchup/Music/Electrify - The Path Untrodden.mp3
-/home/heinzketchup/Music/ColBreakz - 10.000.mp3
-/home/heinzketchup/Music/ColBreakz - Gourmet Race.mp3
-/home/heinzketchup/Music/ColBreakz - 50.000.mp3
-/home/heinzketchup/Music/Creo - Carnivores.mp3
-/home/heinzketchup/Music/Creo - Sphere.mp3
-/home/heinzketchup/Music/Detious Music - Golden Haze.mp3
-/home/heinzketchup/Music/Dimrain47 - At the Speed of Light.mp3
-/home/heinzketchup/Music/Dion Timmer - Shiawase - VIP.mp3
-/home/heinzketchup/Music/dj-Nate - Theory of Everything.mp3
-/home/heinzketchup/Music/dj-Nate - Theory of Everything 2.mp3
-/home/heinzketchup/Music/dj-Nate - Theory of Everything 3.mp3
-/home/heinzketchup/Music/dj-Nate - Thermodynamix.mp3
-/home/heinzketchup/Music/DJVI - Angels.mp3
-/home/heinzketchup/Music/F-777 - Deadlocked.mp3
-/home/heinzketchup/Music/F-777 - Sonic Blaster.mp3
-/home/heinzketchup/Music/F-777 - This Isn't Science Rocket (2015 Edition).mp3
-/home/heinzketchup/Music/F-777 - Space Battle.mp3
-/home/heinzketchup/Music/F-777 - Airborne Robots.mp3
-/home/heinzketchup/Music/F-777 - Ludicrous Speed 1.mp3
-/home/heinzketchup/Music/Hinkik - Outbreaker.mp3
-/home/heinzketchup/Music/Hinkik - Time Leaper.mp3
-/home/heinzketchup/Music/Hinkik - Explorers.mp3
-/home/heinzketchup/Music/MDK - Fingerbang.mp3
-/home/heinzketchup/Music/MDK - Press Start.mp3
-/home/heinzketchup/Music/MDK - Geometry Dash Official Theme Song.mp3
-/home/heinzketchup/Music/meganeko - Moonlight Sonata (3rd Movement).mp3
-/home/heinzketchup/Music/Nighthawk22 - Isolation.mp3
-/home/heinzketchup/Music/TMM43 - Ultimate Destruction.mp3
-/home/heinzketchup/Music/Waterflame - The Race Around the World.mp3
-/home/heinzketchup/Music/Waterflame - Time Machine.mp3
-/home/heinzketchup/Music/Waterflame - Endgame.mp3
-/home/heinzketchup/Music/Waterflame - Electroman Adventures.mp3
-/home/heinzketchup/Music/Waterflame - Blast Processing.mp3
-/home/heinzketchup/Music/Waterflame - Hexagon Force.mp3
-/home/heinzketchup/Music/Waterflame - Geometrical Dominator.mp3
-/home/heinzketchup/Music/Waterflame - Final Battle.mp3
-/home/heinzketchup/Music/Waterflame, F-777, Autodidactic Studios - Moonbeam.mp3
-/home/heinzketchup/Music/zami - Stay Inside Me (Remix).mp3
+/home/heinrich/Music/850907_Hinkik---Explorers-Chiptun.mp3
+/home/heinrich/Music/ColBreakz - 10.000.mp3
+/home/heinrich/Music/ColBreakz - 50.000.mp3
+/home/heinrich/Music/Creo - Sphere.mp3
+/home/heinrich/Music/Detious Music - Golden Haze.mp3
+/home/heinrich/Music/Dimrain47 - At the Speed of Light.mp3
+/home/heinrich/Music/Dion Timmer - Shiawase - VIP.mp3
+/home/heinrich/Music/dj-Nate - Theory of Everything.mp3
+/home/heinrich/Music/dj-Nate - Theory of Everything 2.mp3
+/home/heinrich/Music/dj-Nate - Theory of Everything 3.mp3
+/home/heinrich/Music/dj-Nate - Thermodynamix.mp3
+/home/heinrich/Music/DJVI - Angels.mp3
+/home/heinrich/Music/F-777 - Deadlocked.mp3
+/home/heinrich/Music/F-777 - Sonic Blaster.mp3
+/home/heinrich/Music/F-777 - Space Battle.mp3
+/home/heinrich/Music/F-777 - Airborne Robots.mp3
+/home/heinrich/Music/F-777 - Ludicrous Speed 1.mp3
+/home/heinrich/Music/F-777 - This Isn't Science Rocket (2015 Edition).mp3
+/home/heinrich/Music/Hinkik - Outbreaker.mp3
+/home/heinrich/Music/Hinkik - Time Leaper.mp3
+/home/heinrich/Music/Hinkik - Explorers.mp3
+/home/heinrich/Music/MDK - Fingerbang.mp3
+/home/heinrich/Music/MDK - Press Start.mp3
+/home/heinrich/Music/MDK - Geometry Dash Official Theme Song.mp3
+/home/heinrich/Music/meganeko - Moonlight Sonata (3rd Movement).mp3
+/home/heinrich/Music/Nighthawk22 - Isolation.mp3
+/home/heinrich/Music/TheFatRat - Unity.mp3
+/home/heinrich/Music/TMM43 - Ultimate Destruction.mp3
+/home/heinrich/Music/Waterflame - The Race Around the World.mp3
+/home/heinrich/Music/Waterflame - Time Machine.mp3
+/home/heinrich/Music/Waterflame - Endgame.mp3
+/home/heinrich/Music/Waterflame - Electroman Adventures.mp3
+/home/heinrich/Music/Waterflame - Blast Processing.mp3
+/home/heinrich/Music/Waterflame - Hexagon Force.mp3
+/home/heinrich/Music/Waterflame - Geometrical Dominator.mp3
+/home/heinrich/Music/Waterflame, F-777, Autodidactic Studios - Moonbeam.mp3
+/home/heinrich/Music/zami - Stay Inside Me (Remix).mp3
